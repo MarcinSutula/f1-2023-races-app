21eec14b5e139dd714c7950228778dc315845de1
Marcin Sutula <msutula@esri.pl>
2022-12-07 11:16:17 +0100

    shortened map animation duration

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,6 @@ export const MIN_SCALE: __esri.MapViewConstraints["minScale"] = 80000000;
 
 //Selecting and navigating between races
 export const GO_TO_RACE_ZOOM: number = 7;
-export const GO_TO_RACE_ANIMATION_DURATION: __esri.GoToOptions2D["duration"] = 1700;
+export const GO_TO_RACE_ANIMATION_DURATION: __esri.GoToOptions2D["duration"] = 1250;
 export const GO_TO_RACE_ANIMATION_EASING: __esri.GoToOptions2D["easing"] =
   "ease-in-out";
