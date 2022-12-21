158a8e7a732aea7ca7bdc9bb57598685d97b1f56
Marcin Sutula <msutula@esri.pl>
2022-12-21 15:27:06 +0100

    cleaned DetailsPanel

--- src/components/DetailsPanel.tsx
+++ src/components/DetailsPanel.tsx
@@ -11,7 +11,7 @@ import { lapRecordInfoFormatter } from "../utils/utils";
 import PanelCard from "./PanelCard";
 
 type DetailsPanelProps = {
-  clickedRaceObj: RaceObj | undefined;
+  clickedRaceObj: RaceObj;
   setClickedRaceObj: (raceObj: RaceObj) => void;
   isLoading: boolean;
   setIsLoading: (isLoading: boolean) => void;
@@ -25,68 +25,64 @@ function DetailsPanel({
 }: DetailsPanelProps) {
   return (
     <PanelCard backgroundColor="#100636">
-      {clickedRaceObj && (
-        <>
-          <div className="flex justify-between align-middle mb-2">
-            <RaceCounter selectedRaceOid={clickedRaceObj.OBJECTID} />
-            <NavigationBtnsContainer
-              clickedRaceOid={clickedRaceObj.OBJECTID}
-              setClickedRaceObj={setClickedRaceObj}
-              setIsLoading={setIsLoading}
-              isLoading={isLoading}
-            />
-          </div>
-          <PanelTitle title={clickedRaceObj.name} />
-          <div className="w-5/6 h-36 m-auto mt-7">
-            <PanelImage attribute={clickedRaceObj.city} type="circuit" />
-          </div>
-          <div className="flex h-8 mt-12 justify-between mx-7">
-            <EventDate
-              startTimestamp={clickedRaceObj.start_date}
-              endTimestamp={clickedRaceObj.race_date}
-            />
-            <PanelImage attribute={clickedRaceObj.country} type="flag" />
-          </div>
-          <RaceLocation
-            city={clickedRaceObj.city}
-            country={clickedRaceObj.country}
-          />
+      <div className="flex justify-between align-middle mb-2">
+        <RaceCounter selectedRaceOid={clickedRaceObj.OBJECTID} />
+        <NavigationBtnsContainer
+          clickedRaceOid={clickedRaceObj.OBJECTID}
+          setClickedRaceObj={setClickedRaceObj}
+          setIsLoading={setIsLoading}
+          isLoading={isLoading}
+        />
+      </div>
+      <PanelTitle title={clickedRaceObj.name} />
+      <div className="w-5/6 h-36 m-auto mt-7">
+        <PanelImage attribute={clickedRaceObj.city} type="circuit" />
+      </div>
+      <div className="flex h-8 mt-12 justify-between mx-7">
+        <EventDate
+          startTimestamp={clickedRaceObj.start_date}
+          endTimestamp={clickedRaceObj.race_date}
+        />
+        <PanelImage attribute={clickedRaceObj.country} type="flag" />
+      </div>
+      <RaceLocation
+        city={clickedRaceObj.city}
+        country={clickedRaceObj.country}
+      />
+      <DetailInfo
+        label="Race Distance"
+        info={clickedRaceObj.race_dist.toString()}
+        measure="km"
+        separatorIndex={3}
+      />
+      <DetailInfo
+        label="Circuit Length"
+        info={clickedRaceObj.circuit_length.toString()}
+        measure="km"
+        separatorIndex={1}
+      />
+      <DetailInfo
+        label="Number of Laps"
+        info={clickedRaceObj.laps_num.toString()}
+      />
+      <DetailInfo
+        label="Number of DRS Zones"
+        info={clickedRaceObj.drs_zones_num.toString()}
+      />
+      {clickedRaceObj.lap_record_owner &&
+        clickedRaceObj.lap_record_year &&
+        clickedRaceObj.lap_record_seconds && (
           <DetailInfo
-            label="Race Distance"
-            info={clickedRaceObj.race_dist.toString()}
-            measure="km"
-            separatorIndex={3}
-          />
-          <DetailInfo
-            label="Circuit Length"
-            info={clickedRaceObj.circuit_length.toString()}
-            measure="km"
-            separatorIndex={1}
-          />
-          <DetailInfo
-            label="Number of Laps"
-            info={clickedRaceObj.laps_num.toString()}
-          />
-          <DetailInfo
-            label="Number of DRS Zones"
-            info={clickedRaceObj.drs_zones_num.toString()}
-          />
-          {clickedRaceObj.lap_record_owner &&
-            clickedRaceObj.lap_record_year &&
-            clickedRaceObj.lap_record_seconds && (
-              <DetailInfo
-                label="Lap Record"
-                main
-                info={lapRecordInfoFormatter(
-                  clickedRaceObj.lap_record_owner,
-                  clickedRaceObj.lap_record_year,
-                  clickedRaceObj.lap_record_seconds
-                )}
-              />
+            label="Lap Record"
+            main
+            info={lapRecordInfoFormatter(
+              clickedRaceObj.lap_record_owner,
+              clickedRaceObj.lap_record_year,
+              clickedRaceObj.lap_record_seconds
             )}
-          <CircuitDetailsBtn />
-        </>
-      )}
+          />
+        )}
+      <CircuitDetailsBtn />
     </PanelCard>
   );
 }
